136bc743585f1729589b94ee0e35dafeb11ee266
liaoyuan feng <fengliaoyuan@outlook.com>
2020-06-26 17:15:51 +0800

    test

--- socket_client.py
+++ socket_client.py
@@ -1,3 +1,14 @@
+###################################################################
+#*- 								                            -*#
+#*- coding	: utf-8 	        			                    -*#
+#*- function	: client                                 	    -*#
+#*- serverhost	: 60.205.247.173				                -*#
+#*- port	: 10000 						                    -*#
+#*- author	: fly		        		                        -*#
+#*-			 					                                -*#
+###################################################################
+
+
 import socket
 import threading
 import os
@@ -7,38 +18,75 @@ def recv_msg(s):
     while True:
         try:         # 测试发现，当服务器率先关闭时，这边也会报ConnectionResetError
             response = s.recv(1024)
-            print(response.decode("gbk"))
+            print(response.decode('utf-8'))
         except ConnectionResetError:
             print("服务器关闭，聊天已结束！")
             s.close()
             break
     os._exit(0)
 
-def send_msg(s):
-    print("连接成功！现在可以发送消息！\n")
-    print("输入消息按回车来发送")
-    print("输入esc来退出聊天")
+def upload(s,data):
+    filename = data
+    if os.path.isfile(filename):
+    # 定义定义文件信息。128s表示文件名为128bytes长，l表示一个int或log文件类型，在此为文件大小
+        fileinfo_size = struct.calcsize('128sl')
+        # 定义文件头信息，包含文件名和文件大小
+        fhead = struct.pack('128sl', bytes(os.path.basename(filename).encode('utf-8')),os.stat(filename).st_size)
+        s.send(fhead)
+        print ('client filename: {0}'.format(filename))
+        fp = open(filename, 'rb')
+        while True:
+            data = fp.read(1024)
+            if not data:
+                print ('{0} file send over...'.format(filename))
+                break
+            s.send(data)
+
+
+def download(s,data):
+    print ('Accept the file {}'.format(data))
     while True:
-        msg = input()
-        if msg == "esc":
-            print("你退出了聊天")
-            s.close()
-            break
-        s.send(msg.encode("gbk"))
-    os._exit(0)
+        fileinfo_size = struct.calcsize('128sl')
+        buf = s.recv(fileinfo_size)
+        if buf:
+            filename, filesize = struct.unpack('128sl', buf)
+            fn = filename.strip(str.encode('\00'))
+            new_filename = os.path.join(str.encode('./'), str.encode('new_') + fn)
+            print ('file new name is {0}, filesize if {1}'.format(new_filename, filesize))
 
-
-threads = [threading.Thread(target=recv_msg,args=(socket_client,)), 
-           threading.Thread(target=send_msg,args=(socket_client,))]
-for t in threads:
-    t.start()
-
+            recvd_size = 0  # 定义已接收文件的大小
+            fp = open(new_filename, 'wb')
+            print ("start receiving...")
+            while not recvd_size == filesize:
+                if filesize - recvd_size > 1024:
+                    data = conn.recv(1024)
+                    recvd_size += len(data)
+                else:
+                    data = conn.recv(filesize - recvd_size)
+                    recvd_size = filesize
+                fp.write(data)
+            fp.close()
+            print ("end receive...")
+        #s.close()
+        break
 if __name__ == '__main__':
     socket_client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     addr = ("60.205.247.173", 10000)
     socket_client.connect(addr)
-    print("连接成功！现在可以接收消息！\n")
-    threads = [threading.Thread(target=recv_msg,args=(socket_client,)), 
-               threading.Thread(target=send_msg,args=(socket_client,))]
-    for t in threads:
-        t.start()
+    threading.Thread(target=recv_msg,args=(socket_client,)).start()
+    print("连接成功！现在可以接受消息并通过输入指令进行操作！\n")
+    #threading.Thread(target=send_msg,args=(socket_client,)).start()
+    while True:
+        request = input()
+        socket_client.send(request.encode('utf-8'))
+        cmd,data = request.split(" ")
+        if cmd == 'upload':
+            upload(socket_client,data)
+        elif cmd == 'download':
+            download(socket_client,data)
+        elif cmd == 'chat':
+            pass
+        elif cmd == 'esc':
+            os._exit(0)
+        else:
+            print("An unknown command!")
--- socket_server.py
+++ socket_server.py
@@ -1,30 +1,17 @@
+###################################################################
+#*- 								                            -*#
+#*- coding	: utf-8 	        			                    -*#
+#*- function	: server                                 	    -*#
+#*- serverhost	: 60.205.247.173				                -*#
+#*- port	: 10000 						                    -*#
+#*- author	: fly		        		                        -*#
+#*-			 					                                -*#
+###################################################################
 import get
 import threading
 import os
 import socket
-
-def forward_message(s,addr):
-    while True:
-        try:
-            response = s.recv(1024).decode("gbk")
-            msg = "{} user {} send the message：{}".format(get.get_time(),addr,response)
-            for client in users.values():   # what is users.values ?
-                client.send(msg.encode("gbk"))
-        except ConnectionResetError:
-            print("user{} quit the chat！".format(addr))
-            users.pop(addr)
-            break
-
-
-def accept_connection(s):
-    while True:
-        s,addr =s.accept()  # 接受到了socket连接
-        users[addr]=s
-        number = len(users)
-        print("user {} connect successfully".foramt(addr))
-                # 开启一个新线程
-        threadingThread(target=forward_message,args=(s,addr)).start()
-
+import struct
 
 def start_sever(s):
     server_addr = (get.get_ip(),10000)
@@ -33,6 +20,85 @@ def start_sever(s):
     print("sever is opening...")
     print("if you want to close the sever,please input: stop server")
     threading.Thread(target=accept_connection,args=(s,)).start()
+
+def accept_connection(s):
+    while True:
+        c,addr =s.accept()  # 接受到了socket连接
+        users[addr]=c
+        number = len(users)
+        print("user {} connect successfully".format(addr))
+                # 开启一个新线程
+        threading.Thread(target=each_client,args=(c,addr)).start()
+
+def each_client(s,addr):
+    while True:
+        try:
+            response = s.recv(1024).decode("utf-8")
+            cmd,data = response.split(" ")
+            if cmd == "chat":
+                chat(addr,data)
+            elif cmd == "upload":
+                upload(s,addr,data)
+            elif cmd == "download":
+                download(s,addr,data)
+            else:
+                print("An unknown command!")
+        except ConnectionResetError:
+            print("user{} quit the chat！".format(addr))
+            users.pop(addr)
+            break
+
+def chat(addr,data):
+    msg = "{} user {} send the message：{}".format(get.get_time(),addr,data)
+    print(msg)
+    for client in users.values():   # what is users.values ?
+        client.send(msg.encode("utf-8"))
+
+def upload(s,addr,data):
+    print ('Accept the file {} from {}'.format(data,addr))
+    while True:
+        fileinfo_size = struct.calcsize('128sl')
+        buf = s.recv(fileinfo_size)
+        if buf:
+            filename, filesize = struct.unpack('128sl', buf)
+            fn = filename.strip(str.encode('\00'))
+            new_filename = os.path.join(str.encode('./'), str.encode('new_') + fn)
+            print ('file new name is {0}, filesize if {1}'.format(new_filename, filesize))
+
+            recvd_size = 0  # 定义已接收文件的大小
+            fp = open(new_filename, 'wb')
+            print ("start receiving...")
+            while not recvd_size == filesize:
+                if filesize - recvd_size > 1024:
+                    data = conn.recv(1024)
+                    recvd_size += len(data)
+                else:
+                    data = conn.recv(filesize - recvd_size)
+                    recvd_size = filesize
+                fp.write(data)
+            fp.close()
+            print ("end receive...")
+        #s.close()
+        break
+
+def download(s,addr,data):
+    filename = data
+    if os.path.isfile(filename):
+    # 定义定义文件信息。128s表示文件名为128bytes长，l表示一个int或log文件类型，在此为文件大小
+        fileinfo_size = struct.calcsize('128sl')
+        # 定义文件头信息，包含文件名和文件大小
+        fhead = struct.pack('128sl', bytes(os.path.basename(filename).encode('utf-8')),os.stat(filename).st_size)
+        s.send(fhead)
+        print ('client filename: {0}'.format(filename))
+        fp = open(filename, 'rb')
+        while True:
+            data = fp.read(1024)
+            if not data:
+                print ('{0} file send over...'.format(filename))
+                break
+            s.send(data)
+    #s.close()
+
 
 def close_sever(s):
     for client in users.values():
