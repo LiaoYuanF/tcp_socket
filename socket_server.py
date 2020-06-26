907c056f5916bf2b15a47af70f2d901c4b370b23
liaoyuan feng <fengliaoyuan@outlook.com>
2020-06-26 14:40:25 +0800

    test

--- socket_server.py
+++ socket_server.py
@@ -3,15 +3,12 @@ import threading
 import os
 import socket
 
-socket_server = socket(socket.AF_INET,socket.SOCK_STREAM)
-users = {}
-
 def forward_message(s,addr):
     while True:
         try:
             response = s.recv(1024).decode("gbk")
             msg = "{} user {} send the message：{}".format(get.get_time(),addr,response)
-            for client in users.values():           # what is users.values ?
+            for client in users.values():   # what is users.values ?
                 client.send(msg.encode("gbk"))
         except ConnectionResetError:
             print("user{} quit the chat！".format(addr))
@@ -21,30 +18,30 @@ def forward_message(s,addr):
 
 def accept_connection(s):
     while True:
-        s,addr =s.accept()                          # 接受到了socket连接
-        users[addr]=s               
+        s,addr =s.accept()  # 接受到了socket连接
+        users[addr]=s
         number = len(users)
         print("user {} connect successfully".foramt(addr))
-        # 开启一个新线程
+                # 开启一个新线程
         threadingThread(target=forward_message,args=(s,addr)).start()
 
 
 def start_sever(s):
-        server_addr = (get.get_ip(),10000)
-        s.bind(server_addr)
-        s.listen(5)
-        print("sever is opening...")
-        print("if you want to close the sever,please input: stop server")
-        threading.Thread(target=self.accept_cont,args=(s)).start()
+    server_addr = (get.get_ip(),10000)
+    s.bind(server_addr)
+    s.listen(5)
+    print("sever is opening...")
+    print("if you want to close the sever,please input: stop server")
+    threading.Thread(target=accept_connection,args=(s,)).start()
 
 def close_sever(s):
     for client in users.values():
         client.close()
     s.close()
-    os.exit(0)
+    os._exit(0)
 
 if __name__=="__main__":
-    socket_server = socket(socket.AF_INET,socket.SOCK_STREAM)
+    socket_server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     users = {}
     start_sever(socket_server)
     while True:
